c1a09627b386766f699df14b4ede15f17a6deb25
leoncross <leon@lscross.co.uk>
2019-01-21 19:33:27 +0000

    jasmine init, travis passing

--- spec/TestSpec.js
+++ spec/TestSpec.js
@@ -1,8 +1,8 @@
 describe('HelloWorld',function(){
   var HelloWorld = require('../src/HelloWorld');
+  test = new HelloWorld();
 
   it('retuns hello World', function(){
-    test = new HelloWorld();
     expect(test.hello()).toEqual('Hello World!')
   });
 });
