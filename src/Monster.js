91a0e1a2c9410e7a0052198904a19e75cd7370a4
cesare camurani <cesare.camurani@gmail.com>
2019-01-22 12:22:00 +0000

    Monster Updated

--- src/Monster.js
+++ src/Monster.js
@@ -1,39 +1,17 @@
 
 function Monster() {
   this.zombie = {
-                name: 'Zombie',
+                name: 'zombie',
                 health: 100,
                 armor: 6,
                 strength: 7,
                 dexterity: 5
                 }
-  this.vampyre = {
-                  name: 'Vlad',
-                  health: 100,
-                  armor: 12,
-                  strength: 12,
-                  dexterity: 10
-                  }
-  this.wolf = {
-                name: 'Fenrir',
-                health: 100,
-                armor: 10,
-                strength: 12,
-                dexterity: 10
-              }
+  
 };
 
 Monster.prototype.zombie = function () {
   return this.zombie;
 }
-
-Monster.prototype.vampyre = function () {
-  return this.vampyre;
-}
-
-Monster.prototype.wolf = function () {
-  return this.wolf;
-}
-
 
 module.exports = Monster;
