7fbc0ff6e75328ba1a769caa4b17738ad0e55f45
cesare camurani <cesare.camurani@gmail.com>
2019-01-22 15:17:08 +0000

    Monster receiveDamage created - Monster selectMonster created

--- spec/MonsterSpec.js
+++ spec/MonsterSpec.js
@@ -3,5 +3,21 @@ describe('Monster', function () {
   var Monster = require('../src/Monster');
 
   var monster = new Monster();
+  var zombie = {
+                  name: 'zombie',
+                  health: 100,
+                  armor: 6,
+                  strength: 7,
+                  dexterity: 5
+                  }
+
+  it('monster can receive a damage', function () {
+    monster.receiveDamage(zombie, 20);
+    expect(zombie['health']).toEqual(80);
+  });
+
+  it('selects a monster', function () {
+    expect(monster.selectMonster('zombie')).toEqual(zombie)
+  });
 
 });
--- src/Monster.js
+++ src/Monster.js
@@ -1,23 +1,25 @@
 
 function Monster() {
-  this.zombie = {
-                name: 'zombie',
-                health: 100,
-                armor: 6,
-                strength: 7,
-                dexterity: 5
-                }
+  this.monsters = [{
+                  name: 'zombie',
+                  health: 100,
+                  armor: 6,
+                  strength: 7,
+                  dexterity: 5
+                  }]
 
 };
 
-Monster.prototype.zombie = function () {
-  return this.zombie;
+Monster.prototype.selectMonster = function (monster) {
+  for(i = 0; i < this.monsters.length; i++) {
+    if(this.monsters[i]['name'] === monster) {
+      return this.monsters[i];
+    }
+  }
 };
 
-// Monster.prototype.receiveDamage = function (monster, damage) {
-//   monster['health'] -= damage
-// };
-
-
+Monster.prototype.receiveDamage = function (monster, damage) {
+  monster['health'] -= damage
+};
 
 module.exports = Monster;
