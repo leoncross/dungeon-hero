3ca6d55ea6421f37b1c6eaaae60d75d3bf20bf1a
cesare camurani <cesare.camurani@gmail.com>
2019-01-22 16:06:55 +0000

    Merge branch 'cesare' of https://github.com/leoncross/team-rogue into cesare

---
+++ spec/DiceSpec.js
@@ -0,0 +1,35 @@
+'use strict';
+describe('Dice', function(){
+
+  var Dice = require('../src/Dice');
+  var dice;
+
+  beforeEach(function() {
+    dice = new Dice();
+  });
+
+  it('the object to exist', function(){
+    expect(dice).toBeDefined();
+  });
+
+  describe('rollDice', function() {
+    it('retuns a random number between 1 and 20', function(){
+      spyOn(Math, 'random').and.returnValue(5);
+      spyOn(dice, 'rollDice').and.returnValue(5);
+      expect(dice.rollDice()).toBe(5);
+    });
+  });
+
+  describe('rollBetween', function() {
+    it('return a number between two given values', function(){
+      spyOn(Math, 'random').and.returnValue(5);
+      spyOn(dice, 'rollBetween').and.returnValue(5);
+      expect(dice.rollBetween(4,8)).toBe(5);
+    });
+
+    it('expect to push into the array', function(){
+      dice.rollBetween(4,8);
+      expect(dice._arrayOfNumbers).toEqual([ 4, 5, 6, 7, 8 ]);
+    });
+  });
+});
