0cf277d909a8838155c3636e0f5a7799a39ba876
Darryl Banks <darryl.banks@zombie9.com>
2019-01-23 15:41:38 +0000

    front and back ends have gotten acquainted!

--- src/start.js
+++ src/start.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 
   $( "#start" ).click(function() {
+    var playerName = getElementById('playerName');
+
+    var game = new Game()
+    game.initialize(playerName)
     $( "#startGame" ).submit();
   });
 
