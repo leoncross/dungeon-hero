ae54859b4477861d82d04340a2da7f790e256b39
cesare camurani <cesare.camurani@gmail.com>
2019-01-23 17:05:35 +0000

    RoomSpec updated

--- src/Rooms.js
+++ src/Rooms.js
@@ -6,7 +6,7 @@ function Rooms (player, monsters, combat) {
 
 Rooms.prototype.roomSelect = function () {
   this.zombieRoom()
-  if (this.healthChecker() === true) {
+  if (this.healthChecker()) {
     return this.escapeRoom()
   } else {
     return this.loseGame()
@@ -29,11 +29,7 @@ Rooms.prototype.escapeRoom = function () {
 
 Rooms.prototype.healthChecker = function () {
   let hero = this.player.returnHero()
-  if (hero['health'] > 0) {
-    return true
-  } else {
-    return false
-  }
+  return hero['health'] > 0
 }
 
 module.exports = Rooms
