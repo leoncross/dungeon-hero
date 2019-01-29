805792afa669e1ab1decc0c1107ae56621e747b2
cesare camurani <cesare.camurani@gmail.com>
2019-01-29 15:33:29 +0000

    refactoring

--- src/Monster.js
+++ src/Monster.js
@@ -3,11 +3,11 @@ function Monster () {
   this.monsters = [
     { name: 'Zombie', difficulty: 'easy', specialAttack: 'Deadly Kiss', specialAttackDamage: 3, health: 30, armor: 3, strength: 3, dexterity: 2, weaponMin: 5, weaponMax: 7, stunStatus: false, image: './static/images/zombie.png' },
     { name: 'Bat Swarm', difficulty: 'easy', specialAttack: 'Flying Scourge', specialAttackDamage: 3, health: 55, armor: 2, strength: 2, dexterity: 6, weaponMin: 1, weaponMax: 2, stunStatus: false, image: './static/images/batswarm.png' },
-    { name: 'Skeleton', difficulty: 'medium', specialAttack: 'Necromancer',  specialAttackDamage: 5, health: 50, armor: 1, strength: 4, dexterity: 2, weaponMin: 8, weaponMax: 11, stunStatus: false, image: './static/images/skeleton.png' },
+    { name: 'Skeleton', difficulty: 'medium', specialAttack: 'Necromancer', specialAttackDamage: 5, health: 50, armor: 1, strength: 4, dexterity: 2, weaponMin: 8, weaponMax: 11, stunStatus: false, image: './static/images/skeleton.png' },
     { name: 'Goblin', difficulty: 'medium', specialAttack: 'Fatal Deception', specialAttackDamage: 5, health: 55, armor: 5, strength: 5, dexterity: 4, weaponMin: 7, weaponMax: 9, stunStatus: false, image: './static/images/goblin.png' },
     { name: 'Gorgon', difficulty: 'hard', specialAttack: 'Venomous Snakes', specialAttackDamage: 7, health: 90, armor: 6, strength: 4, dexterity: 6, weaponMin: 9, weaponMax: 15, stunStatus: false, image: './static/images/gorgon.png' },
     { name: 'Shadow Demon', difficulty: 'hard', specialAttack: 'Eternal Agony', specialAttackDamage: 7, health: 110, armor: 6, strength: 5, dexterity: 2, weaponMin: 8, weaponMax: 12, stunStatus: false, image: './static/images/shadowdemon.png' },
-    { name: 'Dragon', difficulty: 'boss', specialAttack:'Black Flame', specialAttackDamage: 10, health: 300, armor: 6, strength: 13, dexterity: 8, weaponMin: 15, weaponMax: 22, stunStatus: false, image: './static/images/dragon.png' },
+    { name: 'Dragon', difficulty: 'boss', specialAttack: 'Black Flame', specialAttackDamage: 10, health: 300, armor: 6, strength: 13, dexterity: 8, weaponMin: 15, weaponMax: 22, stunStatus: false, image: './static/images/dragon.png' },
     { name: 'Trap', difficulty: 'trap', health: 10, armor: 0, strength: 0, dexterity: 0, weaponMin: 25, weaponMax: 25, stunStatus: false, image: './static/images/trap.png' }
   ]
 };
@@ -35,11 +35,11 @@ Monster.prototype.resetMonsters = function () {
   this.monsters = [
     { name: 'Zombie', difficulty: 'easy', specialAttack: 'Deadly Kiss', specialAttackDamage: 3, health: 30, armor: 3, strength: 3, dexterity: 2, weaponMin: 5, weaponMax: 7, stunStatus: false, image: './static/images/zombie.png' },
     { name: 'Bat Swarm', difficulty: 'easy', specialAttack: 'Flying Scourge', specialAttackDamage: 3, health: 55, armor: 2, strength: 2, dexterity: 6, weaponMin: 1, weaponMax: 2, stunStatus: false, image: './static/images/batswarm.png' },
-    { name: 'Skeleton', difficulty: 'medium', specialAttack: 'Necromancer',  specialAttackDamage: 5, health: 50, armor: 1, strength: 4, dexterity: 2, weaponMin: 8, weaponMax: 11, stunStatus: false, image: './static/images/skeleton.png' },
+    { name: 'Skeleton', difficulty: 'medium', specialAttack: 'Necromancer', specialAttackDamage: 5, health: 50, armor: 1, strength: 4, dexterity: 2, weaponMin: 8, weaponMax: 11, stunStatus: false, image: './static/images/skeleton.png' },
     { name: 'Goblin', difficulty: 'medium', specialAttack: 'Fatal Deception', specialAttackDamage: 5, health: 55, armor: 5, strength: 5, dexterity: 4, weaponMin: 7, weaponMax: 9, stunStatus: false, image: './static/images/goblin.png' },
     { name: 'Gorgon', difficulty: 'hard', specialAttack: 'Venomous Snakes', specialAttackDamage: 7, health: 90, armor: 6, strength: 4, dexterity: 6, weaponMin: 9, weaponMax: 15, stunStatus: false, image: './static/images/gorgon.png' },
     { name: 'Shadow Demon', difficulty: 'hard', specialAttack: 'Eternal Agony', specialAttackDamage: 7, health: 110, armor: 6, strength: 5, dexterity: 2, weaponMin: 8, weaponMax: 12, stunStatus: false, image: './static/images/shadowdemon.png' },
-    { name: 'Dragon', difficulty: 'boss', specialAttack:'Black Flame', specialAttackDamage: 10, health: 300, armor: 6, strength: 13, dexterity: 8, weaponMin: 15, weaponMax: 22, stunStatus: false, image: './static/images/dragon.png' },
+    { name: 'Dragon', difficulty: 'boss', specialAttack: 'Black Flame', specialAttackDamage: 10, health: 300, armor: 6, strength: 13, dexterity: 8, weaponMin: 15, weaponMax: 22, stunStatus: false, image: './static/images/dragon.png' },
     { name: 'Trap', difficulty: 'trap', health: 10, armor: 0, strength: 0, dexterity: 0, weaponMin: 25, weaponMax: 25, stunStatus: false, image: './static/images/trap.png' }
   ]
   return this.monsters
