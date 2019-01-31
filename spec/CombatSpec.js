f66ef034a7c8491e88d87960948810245e6318e9
Luca <eto.lucafrancesco@gmail.com>
2019-01-31 10:10:16 +0000

    console logs removed

--- spec/CombatSpec.js
+++ spec/CombatSpec.js
@@ -345,17 +345,15 @@ describe('Combat',function(){
 
   describe('#monsterSpecialAttack', function() {
     it("normal monster special attack", function() {
-      spyOn(dice, "rollDice").and.returnValue(20); 
+      spyOn(dice, "rollDice").and.returnValue(20);
       spyOn(dice, "rollBetween").and.returnValue(5);
       combat.attackSetup([hero, zombie])
-      console.log(zombie);
       expect(combat.monsterAttack()).toEqual(4)
     });
     it(" monster special attack success", function() {
       spyOn(dice, "rollDice").and.returnValue(20); //roll over 19 -> special attack
       spyOn(dice, "rollBetween").and.returnValue(5); //roll 5 min weaponDamage
       combat.attackSetup([hero, zombie])
-      console.log(zombie);
       expect(combat.monsterSpecialAttack()).toEqual(4) //10- 60% damage red
       expect(combat.hero["health"]).toEqual(96)
     });
